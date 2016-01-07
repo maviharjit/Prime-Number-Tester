ea0ea4d75ce526b5b364d233c13e59ec58ae9a02
Prince Mavi <prince@prince.local>
2016-01-07 18:57:25 -0500

    Initial Commit

---
+++ Prime Number Tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Number Tester
+//
+//  Created by Prince Mavi on 07/01/16.
+//  Copyright © 2016 Babaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
